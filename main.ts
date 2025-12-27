3a691ca188c8f26a20cf71682dca9605e2c1c316
Chris Weed <chris@cjweed.com>
2025-12-27 11:11:42 -0600

    Music stops

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ namespace SpriteKind {
     export const EnemyArrow = SpriteKind.create()
 }
 function onFinishOcean (win: boolean) {
+    music.stopAllSounds()
     ocean.destroy()
-game.gameOver(win)
+    game.gameOver(win)
 }
 let activeEnemy = null
 const ocean = new Ocean({ onComplete: onFinishOcean })
