ed22c1e4959dc6fc0e3e068a1496635773badad0
Chris Weed <chris@cjweed.com>
2025-12-27 23:45:48 -0600

    Added tutorial

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
 namespace SpriteKind {
     export const EnemyArrow = SpriteKind.create()
 }
+const scenePage: string = 'tutorial'
+let ocean: Ocean = null
+let tutorial: Tutorial = null
 function onFinishOcean (win: boolean) {
     music.stopAllSounds()
     ocean.destroy()
 game.gameOver(win)
 }
-let activeEnemy = null
-const ocean = new Ocean({ onComplete: onFinishOcean })
+
 game.onUpdate(function () {
     ocean.onUpdate()
 })
+
+switch (scenePage) {
+    case 'tutorial':
+        tutorial = new Tutorial()
+        break;
+    case 'game':
+        ocean = new Ocean({ onComplete: onFinishOcean })
+        break;
+    default:
+        break
+}
